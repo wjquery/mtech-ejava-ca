783ac8172403e3f99c278a9f2c3ed2e027ebcf0c
xiong.gaogao@gmail.com <xiong.gaogao@gmail.com@aa4fa946-59c9-80bb-2eb3-31cc01c2b225>
2014-11-08 14:45:14 +0000

    password encryption added

--- ExamMgmtSystem/src/java/sg/edu/nus/iss/ems/service/UserBean.java
+++ ExamMgmtSystem/src/java/sg/edu/nus/iss/ems/service/UserBean.java
@@ -1,36 +1,43 @@
 package sg.edu.nus.iss.ems.service;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.persistence.TypedQuery;
 import sg.edu.nus.iss.ems.entity.User;
+import sg.edu.nus.iss.ems.util.EncryptionUtils;
 
 @Stateless
 public class UserBean extends GenericDataAccessService<User> {
     
+    private static final Logger LOGGER = Logger.getLogger(GenericDataAccessService.class.getName()); 
     private static final String AUTH = 
             "select u from User u where u.username = :username and u.password = :password and u.status != 0";
     private static final String FIND_ALL = "User.findAll";
     
+    @Override
     public List<User> findAll() {
         return em.createNamedQuery(FIND_ALL, User.class).getResultList();
     }
     
+    @Override
     public void delete(User user) {
         user.setStatus(0);
         super.update(user);
     }
     
     public User authenticate(String username, String password) {
+        String encryptedPwd = EncryptionUtils.encryt(password, username);
         TypedQuery<User> q = em.createQuery(AUTH, User.class)
                 .setParameter("username", username)
-                .setParameter("password", password);
+                .setParameter("password", encryptedPwd);
         
         User user = null;
         try {
             user = q.getSingleResult();
         } catch (Exception e) {
-            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
         }
         return user;
     }
@@ -38,7 +45,7 @@ public class UserBean extends GenericDataAccessService<User> {
     public boolean changePwd(String username, String oldPwd, String newPwd) {
         User user = authenticate(username, oldPwd);
         if (user != null) {
-            user.setPassword(newPwd);
+            user.setPassword(EncryptionUtils.encryt(newPwd));
         }
         return true;
     }
